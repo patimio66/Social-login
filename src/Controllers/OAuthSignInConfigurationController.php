f4423f4f7b3a668300457f9a112cf86bfc38443e
Adam Mańko <adam.maniek247@gmail.com>
2025-01-17 01:33:13 +0100

    module translation added v2

--- src/Controllers/OAuthSignInConfigurationController.php
+++ src/Controllers/OAuthSignInConfigurationController.php
@@ -22,7 +22,7 @@ class OAuthSignInConfigurationController extends FrameworkBundleAdminController
             $errors = $textFormDataHandler->save($textForm->getData());
 
             if (empty($errors)) {
-                $this->addFlash('success', $this->trans('Successful update.', 'Modules.Oauthsignin.Admin', []));
+                $this->addFlash('success', $this->trans('Successful update', 'Modules.Oauthsignin.Admin', []));
 
                 return $this->redirectToRoute('o_auth_sign_in');
             }
